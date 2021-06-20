13e5b5b0a88a7fae06e888c2f5fd614e2b50d9bd
Al Rodriguez <ProgrammerAl@outlook.com>
2021-06-20 16:59:34 -0400

    Display the directories and files on page load

---
+++ FileAndDirectoryBrowserWebApp/FileAndDirectoryBrowserWebApp/FileAndDirectoryBrowserWebApp/ApiResponses/DirectoryInfoResponse.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FileAndDirectoryBrowserWebApp.ApiResponses
+{
+    public class DirectoryInfoResponse
+    {
+        public DirectorySummaryResponse?[]? Directories { get; set; }
+        public FileSummaryResponse?[]? Files { get; set; }
+
+        public class DirectorySummaryResponse
+        {
+            public string? Name { get; set; }
+        }
+
+        public class FileSummaryResponse
+        {
+            public string? Name { get; set; }
+        }
+    }
+}
--- FileAndDirectoryBrowserWebApp/FileAndDirectoryBrowserWebApp/FileAndDirectoryBrowserWebApp/ViewModels/DirectoryViewModel.cs
+++ FileAndDirectoryBrowserWebApp/FileAndDirectoryBrowserWebApp/FileAndDirectoryBrowserWebApp/ViewModels/DirectoryViewModel.cs
@@ -1,24 +1,52 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.Immutable;
 using System.Linq;
 using System.Net.Http;
+using System.Text.Json;
 using System.Threading.Tasks;
 
+using FileAndDirectoryBrowserWebApp.ApiResponses;
 using FileAndDirectoryBrowserWebApp.Options;
-
-using Microsoft.Extensions.Configuration;
 
 namespace FileAndDirectoryBrowserWebApp.ViewModels
 {
     public class DirectoryViewModel : ViewModelBase
     {
+        private static readonly JsonSerializerOptions SerializerOptions = new JsonSerializerOptions
+        {
+            PropertyNamingPolicy = JsonNamingPolicy.CamelCase
+        };
+
         private readonly HostOptions _hostOptions;
+
+        private bool _isErrored;
+        private ImmutableArray<string> _directoryNames;
+        private ImmutableArray<string> _fileNames;
         public DirectoryViewModel(HostOptions hostOptions)
         {
             _hostOptions = hostOptions;
+            _isErrored = false;
+            _directoryNames = ImmutableArray.Create<string>();
+            _fileNames = ImmutableArray.Create<string>();
         }
 
-        public bool IsErrored { get; private set; }
+        public bool IsErrored
+        {
+            get => _isErrored;
+            private set => SetPropertyValue(ref _isErrored, value);
+        }
+        public ImmutableArray<string> DirectoryNames
+        {
+            get => _directoryNames;
+            private set => SetPropertyValue(ref _directoryNames, value);
+        }
+
+        public ImmutableArray<string> FileNames
+        {
+            get => _fileNames;
+            private set => SetPropertyValue(ref _fileNames, value);
+        }
 
         public async Task LoadAsync(string? path)
         {
@@ -26,16 +54,30 @@ namespace FileAndDirectoryBrowserWebApp.ViewModels
             {
                 var endpointUri = GenerateEndpointUrl(path);
                 var result = await httpClient.GetAsync(endpointUri);
-                LoadFromResult(result);
+                await LoadFromResultAsync(result);
             }
         }
 
-        private void LoadFromResult(HttpResponseMessage result)
+        private async Task LoadFromResultAsync(HttpResponseMessage result)
         {
             IsErrored = !result.IsSuccessStatusCode;
 
             if (result.IsSuccessStatusCode)
-            { 
+            {
+                var resultJson = await result.Content.ReadAsStringAsync();
+                var resultInfo = JsonSerializer.Deserialize<DirectoryInfoResponse>(resultJson, SerializerOptions);
+
+                DirectoryNames = resultInfo?.Directories
+                                    ?.Where(x => !string.IsNullOrWhiteSpace(x?.Name))
+                                    .Select(x => x!.Name!)
+                                    .ToImmutableArray()
+                                    ?? ImmutableArray.Create<string>();
+
+                FileNames = resultInfo?.Files
+                                ?.Where(x => !string.IsNullOrWhiteSpace(x?.Name))
+                                .Select(x => x!.Name!)
+                                .ToImmutableArray()
+                                ?? ImmutableArray.Create<string>();
             }
         }
 
